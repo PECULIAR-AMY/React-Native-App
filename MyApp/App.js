76b02cfb11b0e118828ca89fc51f128ad9aeec50
unknown <emmanuelpeculiar645@gmail.com>
2024-12-05 17:37:06 +0100

    new commit

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image, ScrollView} from 'react-native';
-const logoImg = require('./assets/favicon.png')
+
+import { SafeAreaView, StyleSheet,Platform } from 'react-native';
+import PokemonCard from './components/PokemonCard'
 
 
 export default function App() {
+
+  const charmanderData ={
+    name: 'Charmander',
+    image: require('./assets/Chistmas doll.jpg'),
+    type: 'Fire',
+    hp: 39,
+    moves: ['Scratch', 'Ember', 'Grow', 'Leer'],
+    weakness: ['water', 'Rock'],
+  };
+
+
   return (
-    <View style={{ flex: 1, backgroundColor:'plum', padding: 60}}>
-      <ScrollView>
-      <Image source={logoImg} style={{height:200, width: 200}} />
-      <Text>
-      Another writing challenge can be to take the individual sentences in the random paragraph and incorporate a single sentence from that into a new paragraph 
-      to create a short story. 
-      Unlike the random sentence generator, the sentences from the random paragraph will have some connection to one another so it will be a bit different. You also won't
-      know exactly how many sentences will appear in the random paragraph.
-      </Text>
-      <Image source={logoImg} style={{height:200, width: 200}} />
-      </ScrollView>
-    </View>
-  )}
+    <SafeAreaView style={styles.container}>
+    <PokemonCard {...charmanderData}/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === 'android' ? 25 : 0,
+  },
+});
---
+++ MyApp/components/PokemonCard.js
@@ -0,0 +1,59 @@
+import { View, Text, StyleSheet, Platform, Image } from 'react-native'
+
+export default function PokemonCard ({
+    name,
+    image,
+    type,
+    hp,
+    moves,
+    weaknesses
+
+}){
+    return (
+        <View style = {styles.card}>
+            <View>
+                <Text>{name}</Text>
+                <Text>{hp}</Text>
+            </View>
+            <Image source={image} accessibilityLabel={`${name} pokemon`} />
+
+            <View>
+                <Text>{type}</Text>
+            </View>
+
+            <View>
+                <Text>
+                    Moves: {moves.join (", ")}
+                </Text>
+            </View>
+
+            <View>
+                <Text>
+                    weakness: {weaknesses.join (", ")}
+                </Text>
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    card: {
+      backgroundColor: 'White',
+      borderRadius: 16,
+      borderWidth: 2,
+      padding: 16,
+      margin:16,
+      ...Platform.select({
+        ios: {
+            shadowOffset: {width: 2, height: 2},
+            shadowColor: '#333',
+            shadowOpacity: 0.3,
+            shadowRadius: 4,
+        },
+        android: {
+        elevation: 5
+        }
+      })
+
+    }
+})
