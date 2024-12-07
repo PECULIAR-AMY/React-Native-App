f76237ff1e596780654933f2ba748f6f43327bc9
unknown <emmanuelpeculiar645@gmail.com>
2024-12-07 18:46:01 +0100

    new commit

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,5 +1,5 @@
 
-import { SafeAreaView, StyleSheet,Platform } from 'react-native';
+import { SafeAreaView, StyleSheet,Platform, ScrollView } from 'react-native';
 import PokemonCard from './components/PokemonCard'
 
 
@@ -14,11 +14,43 @@ export default function App() {
     weakness: ['water', 'Rock'],
   };
 
+  const squirtleData = {
+    name: 'SquirtleData',
+    image: require('./assets/christmas squirtle.jpeg'),
+    type: 'Water',
+    hp: 44,
+    moves: ['Tackle', 'Water', 'Gun', 'Tailwhip', 'Withdraw'],
+    weakness: ['Electric', 'Grass'],
+  };
+
+  const bulbasaurData = {
+    name: 'SquirtleData',
+    image: require('./assets/Chistmas doll.jpg'),
+    type: 'Grass',
+    hp: 45,
+    moves: ['Tackle', 'vine whip', 'Growl', 'Leach Seed'],
+    weakness: ['Fire', 'ice', 'Flying', 'Psychic'],
+  };
+
+  const santaData = {
+    name: 'SantaData',
+    image: require('./assets/christmas gift.jpg'),
+    type: 'Water',
+    hp: 44,
+    moves: ['Quick Attack', 'Thunderbolt', 'Tail whip', 'Grow'],
+    weakness: ['Ground'],
+  }
+
+
 
   return (
-    <SafeAreaView style={styles.container}>
+    <ScrollView> <SafeAreaView style={styles.container}>
     <PokemonCard {...charmanderData}/>
-    </SafeAreaView>
+    <PokemonCard {...squirtleData}/>
+    <PokemonCard {...bulbasaurData}/>
+    <PokemonCard {...santaData}/>
+    </SafeAreaView></ScrollView>
+    
   );
 }
 
--- MyApp/components/PokemonCard.js
+++ MyApp/components/PokemonCard.js
@@ -1,37 +1,57 @@
 import { View, Text, StyleSheet, Platform, Image } from 'react-native'
 
+const getTypeDetails = (type) => {
+    switch(type.toLowerCase()) {
+        case 'electric':
+        return{ borderColor: '#FFD700', emoji: ","};
+        case "water":
+        return { borderColor: "#6493EA", emoji: "💧"};
+        case "fire":
+        return { borderColor: "#FF5733", emoji: "🔥"};
+        case "grass":
+        return { borderColor: "#66CC66", emoji: "🦗"};
+        default:
+        return { borderColor: "#AOABAO", emoji: ""};                    
+    }
+};
+
 export default function PokemonCard ({
     name,
     image,
     type,
     hp,
     moves,
-    weaknesses
+    weakness
 
 }){
+    const { borderColor, emoji } = getTypeDetails(type);
     return (
         <View style = {styles.card}>
-            <View>
-                <Text>{name}</Text>
-                <Text>{hp}</Text>
+            <View style = {styles.nameContainer}>
+                <Text style={styles.name}>{name}❤️</Text>
+                <Text style = {styles.hp}>{hp}</Text>
             </View>
-            <Image source={image} accessibilityLabel={`${name} pokemon`} />
+             <Image source={image} 
+             style = {styles.image} 
+             resizeMode='contain'
+             accessibilityLabel={`${name} pokemon`} />
 
-            <View>
-                <Text>{type}</Text>
+            <View style = {styles.typeContainer}>
+                <View style ={[styles.badge,{ borderColor}]}>
+                <Text style={styles.typeEmoji} >{emoji}</Text>
+                <Text style ={styles.typeText}>{type}</Text>
+                </View>
+               
             </View>
 
-            <View>
-                <Text>
-                    Moves: {moves.join (", ")}
-                </Text>
+             <View style={styles.movesContainer}>
+            {/* <Text>Moves: {Array.isArray(moves) ? moves.join(", ") : "No moves available"}</Text> {moves} */} <Text style = {styles.movesText}>{moves}</Text>
+
             </View>
 
-            <View>
-                <Text>
-                    weakness: {weaknesses.join (", ")}
-                </Text>
-            </View>
+            <View style ={styles.weaknessContainer}>
+            {/* <Text>Weakness: {Array.isArray(weaknesses) ? weaknesses.join(", ") : "No moves available"}</Text> */} <Text style={styles.weaknessText}>{weakness}</Text> 
+            </View> 
         </View>
     )
 }
@@ -52,8 +72,61 @@ const styles = StyleSheet.create({
         },
         android: {
         elevation: 5
-        }
-      })
+        },
+      }),
 
+    },
+    nameContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 32
+    },
+    name: {
+     fontSize:30,
+     fontWeight: 'bold'    
+    },
+    hp: {
+       fontSize:22 
+    },
+    image: {
+    width: '100%',
+    height:200,
+    marginBottom: 16,
+    },
+    typeContainer: {
+        alignItems: 'center',
+        marginBottom : 40,
+
+    },
+    badge: {
+       flexDirection: 'row', 
+       alignItems: 'center',
+       paddingVertical: 6,
+       paddingHorizontal: 12,
+       borderRadius: 20,
+       borderWidth: 4
+    },
+    typeEmoji: {
+     fontSize: 30,
+     marginRight: 12,
+    },
+    textType: {
+       fontSize: 22,
+       fontWeight:'bold' 
+    },
+    movesContainer: {
+        marginBottom:16 
+    },
+    movesText: {
+       fontSize: 22,
+       fontWeight: 'bold' 
+    },
+    weaknessContainer: {
+      marginBottom: 8,  
+    },
+    weaknessText: {
+        fontSize: 22,
+        fontWeight: 'bold'
     }
-})
+
+});
